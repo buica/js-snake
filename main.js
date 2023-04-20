32fa6f64da966eda5fed0e0422ddc3333d2f0f40
calvin b <calvin.t.bui@gmail.com>
2023-04-20 09:29:03 -0700

    Implement snake eating food and growing. Need to implement game over logic and going through walls next.

--- main.js
+++ main.js
@@ -1,162 +1,167 @@
 const gameBoard = document.getElementById("gameboard");
 const context = gameBoard.getContext('2d')
 const scoreBoard = document.getElementById("scoreboard");
-const bgColor = "purple";  // mmove to stylesheet?
+const bgColor = "navy";  // mmove to stylesheet?
 const tailColor = "green";
 const foodColor = "red";
 
-
 //todo
-// implement eating food and growing
-// implement score
-// implement game over when hitting walls
+    // implement scoreboard
+    // implement game over when snake collides with its tail
+    // allow snake to move through walls to come out other side
+    // add style sheet and make prettier
+//extras
+    // make mobile viewport friendly
+    // speed up when eating every 10 food
+    // add random superfood that gives extra points
 
+let speed = 4; // used as delay val in setInterval to determine snake speed
 let score = 0;
+let snakeSize = 20;
+let gameOver = false;
 
 window.onload = () => {
     gameLoop()
-}
-
-// main game loop
-const gameLoop = function gameLoop() {
-    setInterval(() => {
-        context.clearRect(0, 0, gameBoard.width, gameBoard.height);
-        snake.moveSnake();
-        //eatFood
-        //checkHitWall
-        drawBoard();
-    }, 1000/10) // fps = 1000 / interval
+    console.log("food pos: ", food.x, food.y);
 }
 
 class Snake {
-    constructor(x, y, size) {
+    constructor(x, y, size, color) {
         this.x = x;
         this.y = y;
         this.size = size;
+        this.color = tailColor;
         this.tail = [{x: this.x, y: this.y}] // tail array
-        this.velocityX = 1;
-        this.velocityY = 0;
+        this.directionX = 1;
+        this.directionY = 0;
     }
 
     moveSnake() {
         let newRect
-        if (this.velocityX == 1) { //moving right
+        if (this.directionX == 1) { //moving right
             newRect = {
-                // we move by adding new rect to end of tail's last rect
+                // we move by adding new rect in front of tail array's last element which is head
                 x: this.tail[this.tail.length - 1].x + this.size,
                 y: this.tail[this.tail.length - 1].y
             }
-        } else if (this.velocityX == -1) { // left
+        } else if (this.directionX == -1) { // left
             newRect = {
                 x: this.tail[this.tail.length - 1].x - this.size,
                 y: this.tail[this.tail.length - 1].y
             }
-        } else if (this.velocityY == 1) { // up
+        } else if (this.directionY == 1) { // up
             newRect = {
                 x: this.tail[this.tail.length - 1].x,
                 y: this.tail[this.tail.length - 1].y + this.size
             }
 
-        } else if (this.velocityY == -1) { // down
+        } else if (this.directionY == -1) { // down
             newRect = {
                 x: this.tail[this.tail.length - 1].x,
                 y: this.tail[this.tail.length - 1].y - this.size
             }
         }
 
-        this.tail.shift();
+        this.tail.shift(); // pop first tail element
         this.tail.push(newRect);
+        this.x = this.tail[this.tail.length - 1].x; // update snake's position
+        this.y = this.tail[this.tail.length - 1].y;
+
+        console.log("snake pos: ", snake.x, snake.y);
     }
 }
 
 class Food {
     constructor(x, y, size) {
-        // generate aligned food randomly within game board
-        this.x = Math.floor(Math.random() * gameBoard.width / snake.size) * snake.size;
-        this.y = Math.floor(Math.random() * gameBoard.height / snake.size) * snake.size;
+        // generate aligned food randomly within game board, keeping it off the very edges
+        // make sure food doesnt spawn on top of snake
+        do {
+            this.x = Math.floor(Math.random() * gameBoard.width);
+            this.y = Math.floor(Math.random() * gameBoard.height);
+            this.x -= this.x % 20; // ensure food positions are in multiples of 20
+            this.y -= this.y % 20; // so snake can eat it when they collide
+        } while (snake.x == this.x && snake.y == this.y)
+
         this.size = snake.size;
         this.color = foodColor;
-
-        // make sure food doesnt spawn on top of snake
     }
 }
 
-// initalize snake at random position on board
-const snake = new Snake(
-    25, //todo: randomize snake starting position
-    25,
-    25
-);
-
+// initalize snake near center position on board
+const snake = new Snake(100, 240, snakeSize, tailColor);
 let food = new Food();
 
 
-// controls
-// awsd not registering, figure out why
-document.addEventListener('keydown', (event) => {
-    if (event.key == 'w' || event.key == 'ArrowUp')  {
-        snake.velocityX = 0;
-        snake.velocityY = -1;
-    } else if (event.key == 'a' || event.key == 'ArrowLeft') {
-        snake.velocityX = -1;
-        snake.velocityY = 0;
-    } else if (event.key == 's' || event.key == 'ArrowDown') {
-        snake.velocityX = 0;
-        snake.velocityY = 1;
-    } else if (event.key == 'd' || event.key == 'ArrowRight') {
-        snake.velocityX = 1;
-        snake.velocityY = 0;
+const eatFood = function eatFoodAndGenerateNewFood() {
+    if ((snake.tail[snake.tail.length - 1].x == food.x) &&
+        (snake.tail[snake.tail.length - 1].y == food.y)) {
+            snake.tail[snake.tail.length] = {x: food.x, y: food.y};
+            food = new Food();
+            score++;
     }
-});
+}
 
+
+//Game rendering functions
 // helper function for drawing rectangles on our game board
 function drawRect(x, y, width, height, color) {
     context.fillStyle = color;
     context.fillRect(x, y, width, height);
 }
-
-// draw snake and apple on game board
+// draw gameboard, snake and, apple on game board
 function drawBoard() {
-    drawRect(0, 0, gameBoard.width, gameBoard.height, "purple");
-    drawRect(0, 0, gameBoard.width, gameBoard.height); // redraw game board
+    drawRect(0, 0, gameBoard.width, gameBoard.height, bgColor);
+
     for (let i = 0; i < snake.tail.length; i++) {
         drawRect(
-            snake.tail[i].x + 2.5, // shift each rect by 2px for gap
-            snake.tail[i].y + 2.5,
-            snake.size - 5, // center on rect
-            snake.size - 5,
-            "white"
+            snake.tail[i].x + 2, // add gap between tail
+            snake.tail[i].y + 2,
+            snake.size, // center on rect
+            snake.size,
+            snake.color
         );
     }
 
     drawRect(food.x, food.y, food.size, food.size, food.color);
 }
 
-// add game logic
-// snake body grows when it reaches food
-// game over if hit wall
-//const hitsWall = function checkIfSnakeHitsWall() {
-
-//}
-
-//const eat = function eatFood() {
-
-//}
-
-//const gameLoop = function mainGameLoop() {
-//    setInterval(() => {
-//        context.clearRect(0, 0, gameBoard.width, gameBoard.height);
-//        snake.moveSnake();
-//        eatFood();
-//        hitsWall();
-//        drawBoard();
-//    }, 50); // fps = 1000 / time interval = 20 for us
-//};
 
 
+// controls
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'w' || event.key == 'ArrowUp')  {
+        snake.directionX = 0;
+        snake.directionY = -1;
+        console.log("up");
+    } else if (event.key == 'a' || event.key == 'ArrowLeft') {
+        snake.directionX = -1;
+        snake.directionY = 0;
+        console.log("left");
+    } else if (event.key == 's' || event.key == 'ArrowDown') {
+        snake.directionX = 0;
+        snake.directionY = 1;
+        console.log("down");
+    } else if (event.key == 'd' || event.key == 'ArrowRight') {
+        snake.directionX = 1;
+        snake.directionY = 0;
+        console.log("right");
+    }
+});
 
 
+const update = function updateDisplayforGameLoop() {
+    context.clearRect(0, 0, gameBoard.width, gameBoard.height);
+    snake.moveSnake();
+    eatFood();
+    //checkHitWall
+    drawBoard();
+}
 
-//window.onload = () => {
-//    gameLoop();
-//}
+
+// main game loop
+const gameLoop = function mainGameLoop() {
+    setInterval(() => {
+        update();
+        //drawBoard();
+    }, 1000 / speed) // fps = 1000 / interval
+}
